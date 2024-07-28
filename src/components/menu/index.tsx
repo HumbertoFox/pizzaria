aa0b9431face17da1f77d2e1ae6f34857ac3dc73
Humberto Ribeiro <betofoxmal@gmail.com>
2024-07-28 17:49:15 -0300

    update hover header

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function MenuComponent() {
     return (
         <nav className='flex w-full justify-between font-sans text-sm font-bold sm:hidden'>
-            <Link href={'#promotion'} className='px-2.5 py-1.5'>PROMOÇÕES</Link>
-            <Link href={'#pizzas'} className='px-2.5 py-1.5'>PIZZAS</Link>
-            <Link href={'/'} className='px-2.5 py-1.5'>MASSAS</Link>
-            <Link href={'/'} className='px-2.5 py-1.5'>PRATOS</Link>
-            <Link href={'/'} className='px-2.5 py-1.5'>SOBREMESAS</Link>
-            <Link href={'/'} className='px-2.5 py-1.5'>BEBIDAS</Link>
-            <Link href={'/'} className='px-2.5 py-1.5'>PETISCOS</Link>
+            <Link href={'#promotion'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>PROMOÇÕES</Link>
+            <Link href={'#pizzas'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>PIZZAS</Link>
+            <Link href={'/'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>MASSAS</Link>
+            <Link href={'/'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>PRATOS</Link>
+            <Link href={'/'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>SOBREMESAS</Link>
+            <Link href={'/'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>BEBIDAS</Link>
+            <Link href={'/'} className='px-2.5 py-1.5 hover:bg-green-600 duration-700'>PETISCOS</Link>
         </nav>
     );
 };
