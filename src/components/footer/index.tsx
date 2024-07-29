ca50cb00c855f9bf79e2af0d87e7a45fc44b3c83
Humberto Ribeiro <humbertofox@hotmail.com>
2024-07-29 15:28:29 -0300

    Ajuste footer responsive

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link"
-import { Facebook, Instagram, Map, MessageCircle, PhoneCall, Smartphone, Twitter } from "react-feather";
+import Link from 'next/link';
+import { Facebook, Instagram, Map, MessageCircle, PhoneCall, Smartphone, Twitter } from 'react-feather';
 
 export default function FooterComponent() {
     return (
-        <footer className='w-full flex justify-between text-nowrap xl:text-sm'>
-            <div className="md:text-xs">
+        <footer className='w-full flex justify-between text-nowrap p-2 xl:text-sm sm:flex-col sm:items-center'>
+            <div className='md:text-xs'>
                 <h2>Contato</h2>
                 <Link href={'/'} className='flex items-center'>
                     <PhoneCall />
@@ -19,24 +19,26 @@ export default function FooterComponent() {
                     <p>+55 (81) 0123-4567</p>
                 </Link>
             </div>
-            <address className="md:text-xs">
+            <address className='md:text-xs'>
                 <Map />
                 <p>Endereço:</p>
                 <p>PE-022, 344-Box-B, Nossa senhora da Conceição, Paulista-PE.</p>
                 <p>Paulista, 53.421-420, BR.</p>
                 <p>Contato: 81 98807-5408 - WhatsApp.</p>
             </address>
-            <div className="md:text-xs">
+            <div className='flex flex-col items-center md:text-xs'>
                 <h2>Redes Sociais</h2>
-                <Link href={'/'}>
-                    <Instagram />
-                </Link>
-                <Link href={'/'}>
-                    <Twitter />
-                </Link>
-                <Link href={'/'}>
-                    <Facebook />
-                </Link>
+                <div className='flex gap-4'>
+                    <Link href={'/'}>
+                        <Instagram />
+                    </Link>
+                    <Link href={'/'}>
+                        <Twitter />
+                    </Link>
+                    <Link href={'/'}>
+                        <Facebook />
+                    </Link>
+                </div>
             </div>
         </footer>
     );
