15114ad0fd3dd45a4f3382531738b6381b8008bc
Humberto Ribeiro <humbertofox@hotmail.com>
2024-07-29 03:04:45 -0300

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FooterComponent from '@/components/footer';
 import HeaderComponent from '@/components/header';
 import MenuComponent from '@/components/menu';
 import PizzasComponent from '@/components/pizza';
@@ -5,11 +6,12 @@ import PromotionComponent from '@/components/promotion';
 
 export default function Home() {
   return (
-    <main className='w-10/12 lg:w-full'>
+    <main className='w-10/12 lg:w-full sm:h-dvh'>
       <HeaderComponent />
       <MenuComponent />
       <PromotionComponent />
       <PizzasComponent />
+      <FooterComponent />
     </main>
   );
 };
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link"
+import { Facebook, Instagram, MessageCircle, PhoneCall, Smartphone, Twitter } from "react-feather";
+
+export default function FooterComponent() {
+    return (
+        <footer className='w-full flex justify-between'>
+            <div>
+                <h2>Contato</h2>
+                <Link href={'/'} className='flex items-center'>
+                    <PhoneCall />
+                    <p>(81) 3012-3456</p>
+                </Link>
+                <Link href={'/'} className='flex items-center'>
+                    <Smartphone />
+                    <p>(81) 90123-4567</p>
+                </Link>
+                <Link href={'/'} className='flex items-center'>
+                    <MessageCircle />
+                    <p>+55 (81) 0123-4567</p>
+                </Link>
+            </div>
+            <address>
+                <p>Endereço:</p>
+                <p>PE-022, 344-Box-B, Nossa senhora da Conceição, Paulista-PE.</p>
+                <p>Paulista, 53.421-420, BR.</p>
+                <p>Contato: 81 98807-5408 - WhatsApp.</p>
+            </address>
+            <div>
+                <h2>Redes Sociais</h2>
+                <Link href={'/'}>
+                    <Instagram />
+                </Link>
+                <Link href={'/'}>
+                    <Twitter />
+                </Link>
+                <Link href={'/'}>
+                    <Facebook />
+                </Link>
+            </div>
+        </footer>
+    );
+};
