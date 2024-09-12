d6280ff70e173b282292356affafde6d22baea0c
Humberto Ribeiro <betofoxmal@gmail.com>
2024-09-12 17:54:12 -0300

    update menu ajuste

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -2,33 +2,40 @@
 import { ClickedMenu } from '@/interfaces/interfaces';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-const ClassLinks = 'px-2.5 py-1.5 hover:bg-green-600 duration-700 hover:text-white';
+const CLASS_LINKS = 'px-2.5 py-1.5 hover:bg-green-600 duration-700 hover:text-white';
 
 export default function MenuComponent({ isClicked }: ClickedMenu) {
     const [isVisible, setIsVisible] = useState(isClicked);
-    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 790);
+    const [isWideScreen, setIsWideScreen] = useState<boolean>(true);
 
     useEffect(() => {
         setIsVisible(isClicked);
     }, [isClicked]);
 
     useEffect(() => {
-        const handleResize = () => setIsWideScreen(window.innerWidth > 790);
+        if (typeof window !== 'undefined') {
+            const mediaQuery = window.matchMedia('(min-width: 790px)');
+            setIsWideScreen(mediaQuery.matches);
 
-        window.addEventListener('resize', handleResize);
+            const handleResize = () => setIsWideScreen(mediaQuery.matches);
 
-        return () => window.removeEventListener('resize', handleResize);
+            window.addEventListener('resize', handleResize);
+
+            return () => window.removeEventListener('resize', handleResize);
+        }
     }, []);
 
+    const navClassName = `w-full flex justify-between font-sans text-sm font-bold bg-amber-500 sm:absolute sm:left-0 sm:flex-col sm:z-10 sm:w-32 sm:text-center ${isVisible && !isWideScreen && 'hidden'}`;
+
     return (
-        <nav className={`w-full flex justify-between font-sans text-sm font-bold bg-amber-500 sm:absolute sm:left-0 sm:flex-col sm:z-10 sm:w-32 sm:text-center ${isVisible && !isWideScreen && 'hidden'}`}>
-            <Link href={'#promotion'} className={ClassLinks}>PROMOÇÕES</Link>
-            <Link href={'#pizzas'} className={ClassLinks}>PIZZAS</Link>
-            <Link href={'/'} className={ClassLinks}>MASSAS</Link>
-            <Link href={'/'} className={ClassLinks}>PRATOS</Link>
-            <Link href={'/'} className={ClassLinks}>SOBREMESAS</Link>
-            <Link href={'/'} className={ClassLinks}>BEBIDAS</Link>
-            <Link href={'/'} className={ClassLinks}>PETISCOS</Link>
+        <nav className={navClassName}>
+            <Link href={'#promotion'} className={CLASS_LINKS}>PROMOÇÕES</Link>
+            <Link href={'#pizzas'} className={CLASS_LINKS}>PIZZAS</Link>
+            <Link href={'/'} className={CLASS_LINKS}>MASSAS</Link>
+            <Link href={'/'} className={CLASS_LINKS}>PRATOS</Link>
+            <Link href={'/'} className={CLASS_LINKS}>SOBREMESAS</Link>
+            <Link href={'/'} className={CLASS_LINKS}>BEBIDAS</Link>
+            <Link href={'/'} className={CLASS_LINKS}>PETISCOS</Link>
         </nav>
     );
 };
